709513eb64b1d776b6e5c218e1bfad3db2de5b6e
Marina Barker <marinka-0000@mail.ru>
2023-12-11 12:20:04 +0300

    change2

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,13 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function addCards(name, link, del) {
+function createCards(name, link, del) {
     const cards = cardTemplate.querySelector('.card').cloneNode(true);
     const deleteButton = cards.querySelector('.card__delete-button');
     cards.querySelector('.card__image').src = link;
     cards.querySelector('.card__image').alt = name;
     cards.querySelector('.card__title').textContent = name;
-    deleteButton.addEventListener('click', function(evt){
-        deleteCard(evt);
-    });
+    deleteButton.addEventListener('click', del);
     return cards;
 }
 
@@ -25,5 +23,5 @@ function deleteCard(evt) {
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function(element) {
- placesList.append(addCards(element.name, element.link, element.del));
+ placesList.append(createCards(element.name, element.link, deleteCard));
 })
